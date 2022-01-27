a5f6987b94d2842affde1059b2c9f6cabefb2a0b
Hey <marisantl@hotmail.com>
2022-01-26 23:06:18 -0300

    first

---
+++ exercicio5.js
@@ -0,0 +1,56 @@
+//Array // Crie um array que receba 5 itens e exiba no console.
+
+var Caneta =["Waterman","Montblanc","Bic","fabercastel","Stabilo"];
+console.log(caneta); 
+Caneta = new array ("Waterman","Montblanc","Bic","fabercastel","Stabilo");
+var varioselementos = ["chaves", 5, false, 568, "mais uma coisa"];
+var quartaCaneta = Bic [3];
+console.log(quartaCaneta);
+Caneta[3] = "Bic";
+console.log(canetas);
+
+//propriedade e metodos Array
+
+var pens; 
+
+pens = ["Waterman","Bic","montblanc","fabercastel","Stabilo"];
+
+console.log('Before:pens');
+
+console.log('array length: ", pens.length');
+
+// Utilize um método para adicionar um nome ao inicio do array.
+
+
+var adicionar = caneta.unshift(Waterman)
+console.log(caneta)
+var adicionar = caneta.unshift(Waterman)
+var adicionar =  caneta.unshift(caneta)
+caneta.unshift(Bic)
+console.log(caneta)
+
+// Utilize um método para remover o último nome do array.
+
+var removedItem = caneta.splice("5");
+console.log(Caneta)
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+
+Letnumbers = [1,2,3,4,5];
+
+let numbersAsc = numbers.sort(function(a, b) {
+    return a - b;
+});
+    console.log(Caneta);[1,2,3,4,5] 
+
+
+
+
+
+
+
+
+
+
+
+
